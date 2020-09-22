b591569b77640d823dfa3ae51aa65a2517974b19
prernagaikwad <32159684+prernagaikwad@users.noreply.github.com>
2020-09-22 22:16:42 +0530

    Add files via upload

---
+++ exm1.R
@@ -0,0 +1,47 @@
+n <- c(4,3,5,6,7)
+o <- c(5,8,6,2,1)
+
+#arithmetic operator
+
+x = 1
+y = 1
+x + y
+
+x - y
+x * y
+x % y
+x / y
+n + o
+n - o
+n % o
+x <- 10
+y <- 3
+x %% y
+x / y
+
+q <- c(1, 3, 4, 3, 2) 
+r <- c(2, 4, 4, 5, 2)
+# 1. > greater than 
+q > r
+q>r
+# 2. < lesser than
+q < r
+# 3. == equal to Comparison operator
+q == r
+# 4. >= greater than or equal to
+q >= r
+
+# 5. <= lesser than or equal to 
+q <= r
+
+# 6. != not equal to 
+q != r
+# Assignment Operators
+# 1. Left Assignment <- or == or <<-
+v1 <- c(3,1,TRUE)
+v2 <<- c(3,1,TRUE)
+v3 == c(3,1,FALSE)
+# 2. Right Assignment -> or ->>
+c(3,1,TRUE,2+3i) -> v4
+c(3,1,TRUE,2+3i) ->> v5
+c(4,4,FALSE) ->> v6
